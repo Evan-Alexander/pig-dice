535ebe62fa5cbc658e6272febcc4a65100956e6f
Ethan Luts and Jason Brown <jason@>
2017-01-25 16:44:12 -0800

    add currentturn function to backend for if 1 is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
-var player1;
-var player2;
+//backend
+
+
 
 var dieRoll = function() {
   return Math.floor(Math.random() * 6) + 1;
 };
 
-var tempScore = [];
+var currentTurn = function() {
+  var score = 0;
+  score = dieRoll();
+  if (score != 1) {
+    return score;
+  } else
+  score = 0; //otherwise resets to 0
+}
 
 
+  //for (i = 1; i <= roll; i++)
+
+//var sum = tempScore.reduce(function(first, last) {
+  //return first + last; }, 0);
 
 
-
+//User interface
 $(document).ready(function() {
-  $("#roll1").click(function(event) {
-    event.preventDefault();
-    var roll = dieRoll();
+  $("#roll1").click(function() {
 
-    
+    var roll = parseInt(dieRoll());
+    var turnScore = roll+=roll;
+
     $("#roll-output1").text(roll);
+    $("#turnScore").text(turnScore);
+    console.log(turnScore);
+
+
   });
 });
